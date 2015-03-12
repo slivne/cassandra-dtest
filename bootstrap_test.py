4aca6a264a8525a486382bfb68f1f0b5caee495f
Alan Boudreault <alan@alanb.ca>
2015-03-12 18:39:35 +0000

    Add test for local quorum queries while a node is bootstrapping

--- bootstrap_test.py
+++ bootstrap_test.py
@@ -1,4 +1,7 @@
 import random, time
+import tempfile
+import subprocess
+import re
 from dtest import debug, Tester
 from tools import new_node, insert_c1c2, query_c1c2
 from assertions import assert_almost_equal
@@ -77,3 +80,45 @@ class TestBootstrap(Tester):
         stress_table = 'keyspace1.standard1' if self.cluster.version() >= '2.1' else '"Keyspace1"."Standard1"'
         rows = session.execute('select * from %s limit 10' % stress_table)
         assert len(list(rows)) == 10
+
+    # should be moved in consistent_bootstrap.py or consistency_test.py
+    def local_quorum_bootstrap_test(self):
+        """Test that CL local_quorum works while a node is bootstrapping. CASSANDRA-8058"""
+
+        cluster = self.cluster
+        cluster.populate([1, 1])
+        version = cluster.version()
+        cluster.start()
+
+        node1 = cluster.nodes['node1']
+        if version < "2.1":
+            node1.stress(['-n', '2000000', '-t', '15', '-S', '100',
+                          '--replication-strategy', 'NetworkTopologyStrategy',
+                          '--strategy-properties', 'dc1:1,dc2:1'])
+        else: pass
+            #cursor = self.patient_cql_connection(node1)
+            #cursor.execute("""
+            #  CREATE KEYSPACE "Keyspace1" WITH replication =
+            #  {'class': 'NetworkTopologyStrategy', 'dc1': 1, 'dc2': 1};
+            #""")
+            #node1.stress(['write', 'n=200', '-rate', 'threads=1', '-col', 'size=FIXED(100)'])
+
+        node3 = new_node(cluster, data_center='dc2')
+        node3.start(no_wait=True)
+        time.sleep(3)
+
+        with tempfile.TemporaryFile(mode='w+') as tmpfile:
+            if version < "2.1":
+                node1.stress(['-o', 'insert', '-t', '5', '-e', 'LOCAL_QUORUM'],
+                             stdout=tmpfile, stderr=subprocess.STDOUT)
+            else: pass
+                #node1.stress(['write', 'n=1', 'cl=LOCAL_QUORUM', '-rate', 'threads=8'])
+
+            tmpfile.seek(0)
+            output = tmpfile.read()
+
+        debug(output)
+        regex = re.compile("Operation.+error inserting key.+Exception")
+        failure = regex.search(output)
+        dsds
+        self.assertEqual(failure, None, "Error during stress while bootstrapping")
